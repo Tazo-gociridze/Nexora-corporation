efece36d7d2de5d46d2e1adbdb3cfa4505573d03
Tazo Gociridze <tazogociridze9@gmail.com>
2025-04-16 01:58:24 +0400

    made the header fixed

--- js/script.js
+++ js/script.js
@@ -71,3 +71,15 @@ window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
+
+
+
+    const header = document.querySelector(".header");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 10) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
